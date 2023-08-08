4038842eae5ab6387df7691aa6906a73def671d9
Matias Zanan <matiaszanan@gmail.com>
2023-08-08 15:59:45 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        surface: "#FEFEFE",
+        primary: "#05084E",
+        tertiary: "#F2E3E1",
+        secondary: "#66CCCC",
+        accent: "#DF1C01",
+        text: "#2B2B2B",
+        banner: "#E4938A",
+      },
+    },
+  },
+  plugins: [],
+};
